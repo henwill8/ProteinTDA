a8d9a818a1b5a48ecd3b405f8e629319c53fc79f
Ahmed Guler <abg13099@uga.edu>
2026-06-05 13:47:49 -0400

    Work on converting ESM output to OpenFold output for loss computationx

---
+++ conversions.py
@@ -0,0 +1,28 @@
+from transformers.models.esm.modeling_esmfold import EsmForProteinFoldingOutput
+from sidechainnet_graph import SCNProtein
+
+def out_conversion(
+            esm_out: EsmForProteinFoldingOutput,
+            sc_protein: SCNProtein
+            ):
+    out = {}
+    out["sm"] = {}
+    out["sm"]["frames"] = esm_out.frames
+    out["sm"]["sidechain_frames"] = esm_out.sidechain_frames
+    out["sm"]["positions"] = esm_out.positions
+    out["sm"]["angles"] = esm_out.angles
+    out["sm"]["unnormalized_angles"] = esm_out.unnormalized_angles
+    out["tm_logits"] = esm_out.ptm_logits
+
+    # Unsure if this is exactly right
+    out["lddt_logits"] = esm_out.lddt_head
+
+    # ? final_atom_positions
+    # ? distogram_logits
+    # ? experimentally_resolved_logits
+    # ? masked_msa_logits
+
+    batch = {}
+    
+    # batch from sc_protein
+    return out, batch
